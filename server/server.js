6b3f954e6f91075b1aa1d6bb8234ca7c2c374ee3
TimWeyel <tweyel@gmail.com>
2021-07-10 13:35:35 -0700

    added path to server.js and removed heroku-postbuild script

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const { authMiddleware } = require('./utils/auth');
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
+const path = require('path');
 
-// import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+// const routes = require('./routes');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -20,12 +23,15 @@ const server = new ApolloServer({
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+// app.use(routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
